9e9894962f1e3b9f623b3efeaa507640c142146d
RahulRajGiri15 <160770727+RahulRajGiri15@users.noreply.github.com>
2024-06-28 16:15:08 +0530

    This is for resloving render deploy problem

--- db.js
+++ db.js
@@ -14,13 +14,14 @@ mongoose.connect(mongoURL,{
 const { config } = require('dotenv');
 const mongoose = require('mongoose');
 require ('dotenv').config();
-const MONGODBURL_ONLINE = process.env.MONGODBURL_ONLINE;
+//const MONGODBURL_ONLINE = process.env.MONGODBURL_ONLINE;
 const MONGODB_OFFLINE = process.env.MONGODB_LOCAL
 
 //mongoose.connect('mongodb://localhost:27017/hotels');
-//mongoose.connect(MONGODB_OFFLINE)
+mongoose.connect(MONGODB_OFFLINE)
 //mongoose.connect("mongodb+srv://rrahulrajgiri15:Rahulraj6626@hostel.dmxdk7z.mongodb.net/?retryWrites=true&w=majority&appName=hostel");
-mongoose.connect(MONGODBURL_ONLINE);
+//mongoose.connect(MONGODBURL_ONLINE);
+
 
 ///get the default connection
 ///mongoose maintains a default connection object represting the Mongodb connection
