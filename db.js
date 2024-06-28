7a7f44547b4cf7f15688ba0ba73d17cf0340cb14
RahulRajGiri15 <160770727+RahulRajGiri15@users.noreply.github.com>
2024-06-28 18:16:12 +0530

    new data is being added

--- db.js
+++ db.js
@@ -15,14 +15,14 @@ mongoose.connect(mongoURL,{
 
 const mongoose = require('mongoose');
 require('dotenv').config();
-//const MONGODBURL_ONLINE = process.env.MONGODBURL_ONLINEDATA;
+const MONGODBURL_ONLINE = process.env.MONGODBURL_ONLINE_DATA;
 //const MONGODB_OFFLINE = process.env.MONGODB_LOCAL;
 
 //mongoose.connect('mongodb://localhost:27017/hotels');
-mongoose.connect("mongodb+srv://hoteluser:india6626@hotel.v1gh4on.mongodb.net/?retryWrites=true&w=majority&appName=hotel");
+//mongoose.connect("mongodb+srv://hoteluser:india6626@hotel.v1gh4on.mongodb.net/?retryWrites=true&w=majority&appName=hotel");
 
 //mongoose.connect(MONGODB_OFFLINE)
-//mongoose.connect('MONGODBURL_ONLINE')
+mongoose.connect(MONGODBURL_ONLINE);
 
 
 ///get the default connection
