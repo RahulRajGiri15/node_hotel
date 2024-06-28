ea6f709d03b54fd44a8cb8bb710d1ec4b1c0ecd0
RahulRajGiri15 <160770727+RahulRajGiri15@users.noreply.github.com>
2024-06-28 18:05:08 +0530

    changes made in db file

--- db.js
+++ db.js
@@ -11,18 +11,18 @@ mongoose.connect(mongoURL,{
 })
 */
 
-/*
 
-const { config } = require('dotenv');
+
 const mongoose = require('mongoose');
-require ('dotenv').config();
-//const MONGODBURL_ONLINE = process.env.MONGODBURL_ONLINE;
-const MONGODB_OFFLINE = process.env.MONGODB_LOCAL;
+require('dotenv').config();
+//const MONGODBURL_ONLINE = process.env.MONGODBURL_ONLINEDATA;
+//const MONGODB_OFFLINE = process.env.MONGODB_LOCAL;
 
 //mongoose.connect('mongodb://localhost:27017/hotels');
-mongoose.connect(MONGODB_OFFLINE)
-//mongoose.connect("mongodb+srv://rrahulrajgiri15:Rahulraj6626@hostel.dmxdk7z.mongodb.net/?retryWrites=true&w=majority&appName=hostel");
-//mongoose.connect(MONGODBURL_ONLINE);
+mongoose.connect("mongodb+srv://hoteluser:india6626@hotel.v1gh4on.mongodb.net/?retryWrites=true&w=majority&appName=hotel");
+
+//mongoose.connect(MONGODB_OFFLINE)
+//mongoose.connect('MONGODBURL_ONLINE')
 
 
 ///get the default connection
@@ -45,43 +45,3 @@ db.on('disconnected', ()=>{
 module.exports =db;
 
 
-*/
-
-const mongoose = require('mongoose');
-require('dotenv').config();
-
-// Load MongoDB URI from environment variables
-const MONGODB_OFFLINE = process.env.MONGODB_LOCAL;
-const MONGODB_ONLINE = process.env.MONGODB_URL_ONLINE;
-
-// Use the correct MongoDB URI based on your environment
-const mongoURI = process.env.NODE_ENV === 'production' ? MONGODB_ONLINE : MONGODB_OFFLINE;
-
-if (!mongoURI) {
-    throw new Error('MongoDB URI is not defined in environment variables');
-}
-
-// Set up MongoDB connection
-mongoose.connect(mongoURI, {
-    useNewUrlParser: true,
-    useUnifiedTopology: true,
-});
-
-// Get the default connection
-const db = mongoose.connection;
-
-// Define event listeners for the database connection
-db.on('connected', () => {
-    console.log('MongoDB connected');
-});
-
-db.on('error', (error) => {
-    console.error('MongoDB connection error:', error);
-});
-
-db.on('disconnected', () => {
-    console.log('MongoDB disconnected');
-});
-
-// Export the database connection
-module.exports = db;
